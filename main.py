4527342a4379b948a25db866d299f0ec373ca03d
unknown <abdelmaguid.yasser@gmail.com>
2021-09-29 09:15:28 +0200

    rempoved dotenv from main.py

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ import jwt
 
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
-from dotenv import load_dotenv
 
-load_dotenv('.env_file')
-
+JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
 def _logger():
     '''
